cb8f18425163d50a3e727a33d66b9460793aa2f7
Cristian Martinez <mrkittmm@gmail.com>
2024-10-19 06:34:16 -0300

    Agregué handlebars al proyecto, creé el archivo utils.js, la carpeta 'public', 'views', 'layouts' y los archivos main y home.handlebars. Todo probado y funcionando

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from "./utils.js";
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
-
 
 const app = express();
 const PORT = 8080;
 const HOST = 'localhost';
+
+// Inicializo el motor 
+app.engine('handlebars', handlebars.engine());
+// Indico donde estarán las vistas
+app.set('views', __dirname + '/views');
+// Indico el motor con que quiero renderizar
+app.set('view engine', 'handlebars');
 
 // Configuración del server para recibir objetos JSON
 app.use(express.json());
@@ -15,11 +23,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
+// Ubicación de la carpeta 'public'
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.send(`<h1 style = "color:green">70290-Programación-Backend-I
-        1° preentrega - BIENVENIDOS!!</h1>`)
+// app.get('/', (req, res) => {
+//     res.send(`<h1 style = "color:green">70290-Programación-Backend-I
+//         2° preentrega - BIENVENIDOS!!</h1>`)
+// });
+
+// Prueba de Hbs
+app.get('/', (req, res)=>{
+    res.render('home');
 });
+
 app.listen(PORT, () => {
     console.log(`Server listening in http://${HOST}:${PORT}.`);
 
