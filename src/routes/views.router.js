7f11e419a2d598ea8e329b42739ef5d4e611d3e9
Cristian Martinez <mrkittmm@gmail.com>
2024-10-21 11:13:30 -0300

    Cambios menores y correcciones

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import ProductManager from '../services/ProductManager.js';
+import express from "express";
+import ProductManager from "../services/ProductManager.js";
 
 const router = express.Router();
 
 const productManager = new ProductManager();
 
-router.get('/', async(req, res)=>{
-    
-    const products = await productManager.getProducts();
-        
-    res.render('home', {products})
-})
+router.get("/", async (req, res) => {
+  const products = await productManager.getProducts();
+
+  res.render("home", {
+    products,
+    style: "products.css"
+  });
+});
 
 export default router;
