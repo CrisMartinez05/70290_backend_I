156d7f4cee07e886e9443dfcd382d4ea4c34c8f4
Cristian Martinez <mrkittmm@gmail.com>
2024-10-21 23:26:42 -0300

    Agregué formulario para registro de producto y lógica

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -1,1 +1,41 @@
+// import ProductManager from "../../services/ProductManager.js";
+
+// const productManager = new ProductManager();
+
 const socket = io();
+
+const form = document.getElementById('registerProducts');
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    const data = new FormData(form);
+    console.log(data);
+    const product = {};
+    data.forEach((key, value)=>product[key] = value)
+    console.log("Producto formado");
+    console.log(product);
+    fetch('/api/products', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(product)
+    })
+
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Error en la creación del producto');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Producto creado exitosamente: ', data);
+        
+    })
+    .catch(error => {
+        console.log('Error:', error);
+        
+    })    
+
+    
+})
